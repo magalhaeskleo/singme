ba431ebb781de85135f52674d796e32a4957efb5
kleo <magalhaeskleo@gmail.com>
2020-05-09 10:39:20 -0300

    Ações do evento, add, delete, alter

---
+++ frontend/src/pages/Eventos/Action/AddEvents.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { Add } from '@material-ui/icons';
+import { Fab, Dialog, Snackbar, Fade } from '@material-ui/core';
+
+import ModalCadEvento from '../Tab2/CardEvento/ModalCadEvento';
+import api from '../../../services/Api';
+
+function AddEvents({ daySelected, callbackAddEvent }) {
+  const [open, setOpen] = useState(false);
+  const [openSnk, setOpenSnk] = useState(false);
+
+  async function handleSubmit(form) {
+    const grupo_id = localStorage.getItem('grupo_id');
+    const response = await api.post('/eventos', form, {
+      headers: {
+        Authorization: grupo_id,
+      },
+    });
+
+    if (response.data) {
+      setOpenSnk(true);
+      setOpen(false);
+      callbackAddEvent(true);
+    }
+  }
+
+  const handleClose = () => {
+    setOpenSnk(false);
+  };
+
+  return (
+    <div>
+      <Fab size="small" color="primary" onClick={() => setOpen(!open)}>
+        <Add />
+      </Fab>
+
+      <Dialog open={open}>
+        <ModalCadEvento
+          onClose={() => setOpen(!open)}
+          onSubmit={handleSubmit}
+          daySelected={daySelected}
+        />
+      </Dialog>
+    </div>
+  );
+}
+
+export default AddEvents;
---
+++ frontend/src/pages/Eventos/Action/EditarEvents.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { Edit } from '@material-ui/icons';
+import { IconButton, Dialog } from '@material-ui/core';
+
+import ModalCadEvento from '../Tab2/CardEvento/ModalCadEvento';
+import api from '../../../services/Api';
+
+function EditarEvents({ event, callbackEditEvent }) {
+  const [open, setOpen] = useState(false);
+
+  async function handleSubmit(form) {
+    const grupo_id = localStorage.getItem('grupo_id');
+
+    const response = await api.put(`/eventos/${event.id}`, form, {
+      headers: {
+        Authorization: grupo_id,
+      },
+    });
+
+    if (response.data) {
+      callbackEditEvent(true);
+      setOpen(false);
+    }
+  }
+
+  return (
+    <div>
+      <IconButton size="small" onClick={() => setOpen(!open)}>
+        <Edit fontSize="small" />
+      </IconButton>
+
+      <Dialog open={open}>
+        <ModalCadEvento
+          onClose={() => setOpen(!open)}
+          onSubmit={handleSubmit}
+          event={event}
+        />
+      </Dialog>
+    </div>
+  );
+}
+
+export default EditarEvents;
